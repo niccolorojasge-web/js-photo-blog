d64ce013874c0a5d5f97fa11342b0192c6b7f752
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2026-01-08 12:38:59 +0100

    contenuto array

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ const outputEndpoint = "https://lanciweb.github.io/demo/api/pictures/"
 //creiamo la chiamata axios 
 axios.get(outputEndpoint)
     .then(response => {
-const post = response.data;
-//console.log(post)
+        const posts = response.data;
+        //console.log(posts)
+        let postOutput = "";
+        posts.forEach(post => {
+            const title = post.title
+            const date = post.date
+            postOutput += `
+            <div class="card">
+            <h2>${title}</h2>
+            <p>${date}</p>
+        </div>`
+
+
+        });
+        output.innerHTML = postOutput
     })
     .catch()
 
